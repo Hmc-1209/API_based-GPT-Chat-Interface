7e255ef810a44f7e0dc78e423e39dfc28711d71a
Danny Ho <113899395+DannyHo1209@users.noreply.github.com>
2025-02-24 12:44:07 +0800

    FIX: Fix the forget text size issue for mid screen devices

--- app/src/components/chat-section.jsx
+++ app/src/components/chat-section.jsx
@@ -97,7 +97,7 @@ const ChatSection = () => {
   return (
     <>
       {loadingChatData ? (
-        <div className="relative w-screen xl:w-[100%] h-[90%] bg-gray-800 text-md xl:text-[20px] text-gray-500 italic text-center flex items-center justify-center select-none">
+        <div className="relative w-screen md:w-[100%] h-[90%] bg-gray-800 text-md xl:text-[20px] text-gray-500 italic text-center flex items-center justify-center select-none">
           Loading chat data...
         </div>
       ) : (
